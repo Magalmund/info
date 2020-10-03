0ba80f5a2ab2d5a58ae8a1745755779961e732f0
Magalmund <poker6-96@mail.ru>
2020-10-04 02:22:43 +0300

    change

--- page.php
+++ page.php
@@ -7,7 +7,8 @@
 <div class="container-fluid">
     <div class="row">
         <div id="news" class="col-xl-7">
-            <div class="frame">
+            <div class="news-frame">
+                <div class="frame">
                 <h1>Uudised</h1>
                 <?php $catquery = new WP_Query( 'cat=3&posts_per_page=3' ); ?>
                     <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
@@ -20,6 +21,36 @@
                     </div>
                     <?php endwhile; ?>
                 <?php wp_reset_postdata(); ?>
+                </div>
+            </div>
+            <div class="news-frame">
+                <div class="frame row">
+                <div class="col-xl-8">
+                    <div id="logo">
+                        <div class="frame">
+                            <a class="padding-logo">
+                                <span id="image-logo" class="image">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="timedate">
+                        <div class="frame">
+                        </div>
+                    </div>
+                    <div id="weather">
+                        <div class="frame">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4">
+                    <div id="lessons">
+                        <div class="frame">
+                            <h3>Koolitundid</h3>
+                        </div>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
         <div id="changes" class="col-xl-5">
